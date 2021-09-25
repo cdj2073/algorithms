9fb923fbf058068add4d7b41381f7ac8bbf73530
cdj2073 <ab8922@naver.com>
2021-09-26 03:13:25 +0900

    strassen

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -51,8 +51,18 @@ int **add_matrix(int **A, int **B, int n){
 			addition++;
 		}
 	}
-	free_matrix(A, n);
-	free_matrix(B, n);
+	return C;
+}
+
+int **sub_matrix(int **A, int **B, int n){
+	int **C = create_matrix(n);
+
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			C[i][j] = A[i][j] - B[i][j];
+			subtraction++;
+		}
+	}
 	return C;
 }
 
@@ -104,6 +114,23 @@ int **divide_and_conquer_algorithm(int **A, int **B, int n){
 			}
 		}
 
+/*
+//  	memory leak??????
+		int **P1 = divide_and_conquer_algorithm(A11, B11, n / 2);
+		int **P2 = divide_and_conquer_algorithm(A12, B21, n / 2);
+		int **P3 = divide_and_conquer_algorithm(A11, B12, n / 2);
+		int **P4 = divide_and_conquer_algorithm(A12, B22, n / 2);
+		int **P5 = divide_and_conquer_algorithm(A21, B11, n / 2);
+		int **P6 = divide_and_conquer_algorithm(A22, B21, n / 2);
+		int **P7 = divide_and_conquer_algorithm(A21, B12, n / 2);
+		int **P8 = divide_and_conquer_algorithm(A22, B22, n / 2);
+
+		int **C11 = add_matrix(P1, P2, n / 2);
+		int **C12 = add_matrix(P3, P4, n / 2);
+		int **C21 = add_matrix(P5, P6, n / 2);
+		int **C22 = add_matrix(P7, P8, n / 2);
+*/
+
 		int **C11 = add_matrix(divide_and_conquer_algorithm(A11, B11, n / 2), divide_and_conquer_algorithm(A12, B21, n / 2), n / 2);
 		int **C12 = add_matrix(divide_and_conquer_algorithm(A11, B12, n / 2), divide_and_conquer_algorithm(A12, B22, n / 2), n / 2);
 		int **C21 = add_matrix(divide_and_conquer_algorithm(A21, B11, n / 2), divide_and_conquer_algorithm(A22, B21, n / 2), n / 2);
@@ -118,7 +145,6 @@ int **divide_and_conquer_algorithm(int **A, int **B, int n){
 			}
 		}
 
-		/*
 		if (n > 2){
 			printf("Partial matrix of C\n");
 			printf("C11\n");
@@ -130,7 +156,6 @@ int **divide_and_conquer_algorithm(int **A, int **B, int n){
 			printf("C22\n");
 			print_matrix(C22, n / 2);
 		}
-		*/
 
 		free_matrix(A11, n / 2);
 		free_matrix(A12, n / 2);
@@ -145,13 +170,111 @@ int **divide_and_conquer_algorithm(int **A, int **B, int n){
 		free_matrix(C21, n / 2);
 		free_matrix(C22, n / 2);
 
+/*
+		free_matrix(P1, n / 2);
+		free_matrix(P2, n / 2);
+		free_matrix(P3, n / 2);
+		free_matrix(P4, n / 2);
+		free_matrix(P5, n / 2);
+		free_matrix(P6, n / 2);
+		free_matrix(P7, n / 2);
+		free_matrix(P8, n / 2);
+*/
+
 		return C;
 	}
 }
 
 
-void strassen_algorithm(int **A, int **B, int **C, int n){
+int **strassen_algorithm(int **A, int **B, int n){
+	int **C = create_matrix(n);
 
+	if (n == 1){
+		C[0][0] = A[0][0] * B[0][0];
+		multiplication++;
+		return C;
+	}
+	else{
+		int **A11 = create_matrix(n / 2);
+		int **A12 = create_matrix(n / 2);
+		int **A21 = create_matrix(n / 2);
+		int **A22 = create_matrix(n / 2);
+		int **B11 = create_matrix(n / 2);
+		int **B12 = create_matrix(n / 2);
+		int **B21 = create_matrix(n / 2);
+		int **B22 = create_matrix(n / 2);
+
+		for (int i = 0; i < n / 2; i++){
+			for (int j = 0; j < n / 2; j++){
+				A11[i][j] = A[i][j];
+				A12[i][j] = A[i][n / 2 + j];
+				A21[i][j] = A[n / 2 + i][j];
+				A22[i][j] = A[n / 2 + i][n / 2 + j];
+				B11[i][j] = B[i][j];
+				B12[i][j] = B[i][n / 2 + j];
+				B21[i][j] = B[n / 2 + i][j];
+				B22[i][j] = B[n / 2 + i][n / 2 + j];
+			}
+		}
+
+		int **P1 = strassen_algorithm(A11, sub_matrix(B12, B22, n / 2), n / 2);
+		int **P2 = strassen_algorithm(add_matrix(A11, A12, n / 2), B22, n / 2);
+		int **P3 = strassen_algorithm(add_matrix(A21, A22, n / 2), B11, n / 2);
+		int **P4 = strassen_algorithm(A22, sub_matrix(B21, B11, n / 2), n / 2);
+		int **P5 = strassen_algorithm(add_matrix(A11, A22, n / 2), add_matrix(B11, B22, n / 2), n / 2);
+		int **P6 = strassen_algorithm(sub_matrix(A12, A22, n / 2), add_matrix(B21, B22, n / 2), n / 2);
+		int **P7 = strassen_algorithm(sub_matrix(A11, A21, n / 2), add_matrix(B11, B12, n / 2), n / 2);
+
+		int **C11 = add_matrix(P5, sub_matrix(P4, sub_matrix(P2, P6, n / 2), n / 2), n / 2);
+		int **C12 = add_matrix(P1, P2, n / 2);
+		int **C21 = add_matrix(P3, P4, n / 2);
+		int **C22 = add_matrix(P5, sub_matrix(P1, add_matrix(P3, P7, n / 2), n / 2), n / 2);
+
+		for (int i = 0; i < n / 2; i++){
+			for (int j = 0; j < n / 2; j++){
+				C[i][j] = C11[i][j];
+				C[i][n / 2 + j] = C12[i][j];
+				C[n / 2 + i][j] = C21[i][j];
+				C[n / 2 + i][n / 2 + j] = C22[i][j];
+			}
+		}
+
+		if (n > 2){
+			printf("Partial matrix of C\n");
+			printf("C11\n");
+			print_matrix(C11, n / 2);
+			printf("C12\n");
+			print_matrix(C12, n / 2);
+			printf("C21\n");
+			print_matrix(C21, n / 2);
+			printf("C22\n");
+			print_matrix(C22, n / 2);
+		}
+
+
+		free_matrix(A11, n / 2);
+		free_matrix(A12, n / 2);
+		free_matrix(A21, n / 2);
+		free_matrix(A22, n / 2);
+		free_matrix(B11, n / 2);
+		free_matrix(B12, n / 2);
+		free_matrix(B21, n / 2);
+		free_matrix(B22, n / 2);
+		free_matrix(C11, n / 2);
+		free_matrix(C12, n / 2);
+		free_matrix(C21, n / 2);
+		free_matrix(C22, n / 2);
+
+		free_matrix(P1, n / 2);
+		free_matrix(P2, n / 2);
+		free_matrix(P3, n / 2);
+		free_matrix(P4, n / 2);
+		free_matrix(P5, n / 2);
+		free_matrix(P6, n / 2);
+		free_matrix(P7, n / 2);
+
+		return C;
+	}
 }
 
 void print_result(int **A, int **B, int **C, int n){
@@ -165,6 +288,7 @@ void print_result(int **A, int **B, int **C, int n){
 	printf("The number of multiplication : %d\n", multiplication);
 	printf("The number of subtraction : %d\n", subtraction);
 	printf("The number of addition : %d\n", addition);
+	printf("\n");
 }
 
 int main(){
@@ -181,33 +305,34 @@ int main(){
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== standard algorithm =====\n");
+	printf("===== standard algorithm =====\n");
 	C1 = standard_algorithm(A, B, size);
 	print_result(A, B, C1, size);
 
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== divide-and-conquer algorithm =====\n");
+	printf("===== divide-and-conquer algorithm =====\n");
 	C2 = divide_and_conquer_algorithm(A, B, size);
 	print_result(A, B, C2, size);
 
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== strassen algorithm =====\n");
-//	strassen_algorithm(A, B, C, size);
-//	print_result(A, B, C, size);
+	printf("===== strassen algorithm =====\n");
+	C3 = strassen_algorithm(A, B, size);
+	print_result(A, B, C3, size);
 
 	free_matrix(A, size);
 	free_matrix(B, size);
 	free_matrix(C1, size);
 	free_matrix(C2, size);
+	free_matrix(C3, size);
 
 
 	// 8x8 matrix
 	size = 8;
-	printf("\n===== 8x8 matrix =====\n");
+	printf("===== 8x8 matrix =====\n");
 	A = create_matrix(size);
 	B = create_matrix(size);
 
@@ -216,26 +341,27 @@ int main(){
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== standard algorithm =====\n");
+	printf("===== standard algorithm =====\n");
 	C1 = standard_algorithm(A, B, size);
 	print_result(A, B, C1, size);
 
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== divide-and-conquer algorithm =====\n");
+	printf("===== divide-and-conquer algorithm =====\n");
 	C2 = divide_and_conquer_algorithm(A, B, size);
 	print_result(A, B, C2, size);
 
 	multiplication = 0;
 	subtraction = 0;
 	addition = 0;
-	printf("\n===== strassen algorithm =====\n");
-//	strassen_algorithm(A, B, C, size);
-//	print_result(A, B, C, size);
+	printf("===== strassen algorithm =====\n");
+	C3 = strassen_algorithm(A, B, size);
+	print_result(A, B, C3, size);
 
 	free_matrix(A, size);
 	free_matrix(B, size);
 	free_matrix(C1, size);
 	free_matrix(C2, size);
+	free_matrix(C3, size);
 }
